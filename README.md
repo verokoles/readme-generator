14b7fd036c26f8301df361c2edfd879111fb0d40
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-08 00:14:00 -0500

    format README.md

--- README.md
+++ README.md
@@ -1,31 +1,34 @@
-# Professional README Generator::arrow_down:
-  > ## creating a readme markdown file using node from the command line
-   - ## Table of Contents::mag:
-    1. [ Installation ](#installation)
-    2. [ Usage ](#usage)
-    3. [ License ](#license)
-    4. [ Contributing ](#contributing)
-    5. [ Tests ](#tests) 
-    6. [ Questions? ](#questions)
+# PRO README::arrow_down: 
+ 
+  
+  > ## creating readme markdown with node thorugh coman dline
+    ## Table of Contents::mag:
+      1. [ Installation ](#installation)
+      2. [ Usage ](#usage)
+      3. [ License ](#license)
+      4. [ Contributions ](#contributing)
+      5. [ Tests ](#tests) 
+      6. [ Questions? ](#reach-out)
 
-  - ## 'npm install' & 'npm require'
-  - ## undefined
-  - ## MIT
-  - ## undefined 
-  - 
-   @Xandromus: Xander Rapstine
-   -
-  - 
-  @verokoles: Veronica Kolesnikov
-  -
-  - 
-  @gchoi2u: Grace Choi
-  -
+   ## Installation:
+   -  npm install and npm require
+   ## Usage:
+   -  undefined
+   ## License:
+   -  Apache-2.0
+   ## Contributions:
+   -  @Xandromous and @gchoi2u 
+     1.  @Xandromus: Xander Rapstine
+     2.  @verokoles: Veronica Kolesnikov 
+     3.  @gchoi2u: Grace Choi
+  
+   ## Tests:
+   -  node index.js
 
-  - undefined
    ##:raising_hand: Questions? Reach out!:raising_hand:
-  - ### Github:octocat:: @verokoles
-  - ### email:email:: verokoles88@gmail.com
+     ### Github:octocat:: verokoles
+     ### email:email:: verokoles88@gmail.com
 
-MIT
+Apache-2.0
+
 [link to README tutorial video]()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,8 +4,8 @@ function renderLicenseBadge(license) {
   // if ('Apache-2.0')
   // [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
   //(https://opensource.org/licenses/Apache-2.0)
-} else {
-  return ''
+// } else {
+//   return ''
 }
 
 // TODO: Create a function that returns the license link
@@ -13,47 +13,54 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   return `${license}`
   // link: (https://opensource.org/licenses/Apache-2.0)
-} else {
-  return ''
+// } else {
+//   return ''
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   // require("potential-enigma/utils/LICENSE.txt")
-} else {
-  return ''
+// } else {
+//   return ''
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}::arrow_down:
-  ${renderLicenseBadge(license)}
-  > ## ${data.description}
-   - ## Table of Contents::mag:
-    1. [ Installation ](#installation)
-    2. [ Usage ](#usage)
-    3. [ License ](#license)
-    4. [ Contributing ](#contributing)
-    5. [ Tests ](#tests) 
-    6. [ Questions? ](#reach-out)
-
-  - ## ${data.installation}
-  - ## ${data.usage}
-  - ## ${data.license}
-  - ## ${data.contributing} 
-   - @Xandromus: Xander Rapstine
-   - @verokoles: Veronica Kolesnikov 
-   - @gchoi2u: Grace Choi
+   // license badge to appear at top pf page
+  return `# ${data.title}::arrow_down: 
+ 
   
+  > ## ${data.description}
+    ## Table of Contents::mag:
+      1. [ Installation ](#installation)
+      2. [ Usage ](#usage)
+      3. [ License ](#license)
+      4. [ Contributions ](#contributing)
+      5. [ Tests ](#tests) 
+      6. [ Questions? ](#reach-out)
 
-  - ${data.tests}
-  - ${data.reach - out}
+   ## Installation:
+   -  ${data.installation}
+   ## Usage:
+   -  ${data.usage}
+   ## License:
+   -  ${data.license}
+   ## Contributions:
+   -  ${data.contributing} 
+     1.  @Xandromus: Xander Rapstine
+     2.  @verokoles: Veronica Kolesnikov 
+     3.  @gchoi2u: Grace Choi
+  
+   ## Tests:
+   -  ${data.tests}
+
    ##:raising_hand: Questions? Reach out!:raising_hand:
-  - ### Github:octocat:: ${data.github}
-  - ### email:email:: ${data.email}
+     ### Github:octocat:: ${data.github}
+     ### email:email:: ${data.email}
 
 ${renderLicenseLink(data.license)}
+
 [link to README tutorial video]()
 `;
 }
